1af994304a4016461ab4a69bc9de96e2add19a03
Favour <ayofavour7@gmail.com>
2019-03-30 08:31:13 +0100

    Updated Base Modulos
    Added a branch for mod 7 and mod 21

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,15 @@
 def FizzBuzz():
      for i in range(101):
-          if i%15 == 0:
+          if i%21 == 0:
+               print("FizzBuzzWoof")
+          elif i%15 == 0:
                print("FizzBuzz")
           elif i%5 == 0:
                print("Buzz")
           elif i%3 == 0:
                print("Fizz")
+          elif i%7 == 0:
+               print("Woof")
           else:
               print(i)
 
